3bcd396a8b633965868b5e901dff41c2f748cd5b
Ajin Abraham <ajin25@gmail.com>
2020-07-24 19:01:34 -0400

    test for standards

---
+++ tests/unit/test_standards.py
@@ -0,0 +1,25 @@
+"""Unit Tests - test standards mapping."""
+from pathlib import Path
+
+import libsast
+
+
+def test_standards_mapping():
+    a10 = 'A10: Insufficient Logging & Monitoring'
+    m1 = 'M1: Improper Platform Usage'
+    base_dir = Path(__file__).parents[0]
+    files_dir = base_dir / 'assets' / 'files'
+    rules_dir = base_dir / 'assets' / 'rules' / 'pattern_matcher'
+    options = {'match_rules': rules_dir.as_posix()}
+    paths = [files_dir.as_posix()]
+    res = libsast.Scanner(options, paths).scan()
+    match = res['pattern_matcher']['test_regex_multiline']
+    assert match
+    assert match['files'][0]['match_position']
+    assert match['files'][0]['match_lines']
+    assert match['files'][0]['file_path']
+    assert match['metadata']
+    assert match['metadata']['cwe'] == 'cwe-1002'
+    assert match['metadata']['owasp-mobile'] == m1
+    assert match['metadata']['owasp-web'] == a10
+    assert match['metadata']['masvs'] == 'MSTG-STORAGE-3'
