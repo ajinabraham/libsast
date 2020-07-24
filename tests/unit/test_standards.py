363ba0ae4835d5c2621c0be51b527425c02c81ec
Ajin Abraham <ajin25@gmail.com>
2020-07-24 19:19:03 -0400

    fix test

--- tests/unit/test_standards.py
+++ tests/unit/test_standards.py
@@ -15,9 +15,6 @@ def test_standards_mapping():
     res = libsast.Scanner(options, paths).scan()
     match = res['pattern_matcher']['test_regex_multiline']
     assert match
-    assert match['files'][0]['match_position']
-    assert match['files'][0]['match_lines']
-    assert match['files'][0]['file_path']
     assert match['metadata']
     assert match['metadata']['cwe'] == 'cwe-1002'
     assert match['metadata']['owasp-mobile'] == m1
